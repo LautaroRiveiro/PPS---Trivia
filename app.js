b560ae540b79e07a1ebf411c8da6397ea64099fa
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2016-09-05 10:45:53 -0300

    Sexto Paso
    Programé el botón enviar:
    Recorre el objeto respuestasCorrectas y compara cada elemento con el
    correspondiente de respuestasElegidas. Si son iguales, suma un punto.
    Al final muestra un detalle de los aciertos y la cantidad de preguntas.
    
    Nota: Por lo visto JS puede realizar la comparación de dos objetos donde
    el índice indicado de uno de ellos no existe. Pero no sé si está bien
    dejarlo así. Por ejemplo: Si yo no elijo ninguna respuesta (radio
    button), ese elemento no se crea en el objeto respuestasElegidas.

--- app.js
+++ app.js
@@ -5,17 +5,35 @@ app.controller('miController', function($scope, $timeout){
     var preguntasRef = new Firebase('https://triviapp-c9b77.firebaseio.com/preguntas');
     
     $scope.infofirebase = [];
+    $scope.respuestasCorrectas = {};
+    $scope.respuestasElegidas = {};
     
     preguntasRef.on('child_added', function(snapshot){
         $timeout(function(){
             var pregunta = snapshot.val();
             $scope.infofirebase.push(pregunta);
+            $scope.respuestasCorrectas[pregunta.id] = pregunta.respuesta;
         });
-    });
-    
-    $scope.respuesta = {};
+    });    
     
     $scope.Enviar = function(){
-        alert("Sus respuestas fueron: " + JSON.stringify($scope.respuesta));
+        //alert("Sus respuestas fueron: " + JSON.stringify($scope.respuestasElegidas));
+        var contadorAciertos = 0;
+        var contadorPreguntas = 0;
+        
+        for(var respuesta in $scope.respuestasCorrectas){
+            
+            contadorPreguntas ++;
+            
+            if ($scope.respuestasCorrectas[respuesta] === $scope.respuestasElegidas[respuesta]){
+                alert("Respuesta "+respuesta+": BIEN");
+                contadorAciertos ++;
+            }
+            else{
+                alert("Respuesta "+respuesta+": MAL");   
+            }
+        }
+        
+        alert("Cantidad de aciertos: "+contadorAciertos+"/"+contadorPreguntas);
     }
 });
