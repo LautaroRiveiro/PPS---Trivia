eab3ec00ec99cf6b30c09e3ab89a8e56637a5c22
Lautaro Riveiro <lautaroriveiro91@gmail.com>
2016-09-04 21:52:23 -0300

    Tercer paso
    Ahora el JSON de respuestas toma como key el valor del número de
    pregunta.
    Para hacerlo tuve que agregarle una nueva key a los JSON de preguntas,
    por lo que tuve que agregar un selector en el ng-repeat

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.controller('miController', function($scope){
     $scope.opciones = ['Azul y Amarillo', 'Rojo y Blanco', 'Verde'];
     $scope.nombres = ['Juanma', 'Facu', 'Lautaro'];
     
+    $scope.preguntas = {
+        opciones : {numero: 1, opciones: ['Azul y Amarillo', 'Rojo y Blanco', 'Verde']},
+        nombres : {numero: 2, opciones: ['Juanma', 'Facu', 'Lautaro']}
+    };
+    
     $scope.respuesta = {};
     
     $scope.Enviar = function(){
